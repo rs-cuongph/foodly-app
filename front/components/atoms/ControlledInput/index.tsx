cc14997d28ecb24c378ae21d5ec0c7f54694efdc
Anh-Khanh <leanhkhanh6540@gmail.com>
2024-07-15 19:43:12 +0700

    feat: fix

--- front/components/atoms/ControlledInput/index.tsx
+++ front/components/atoms/ControlledInput/index.tsx
@@ -17,6 +17,7 @@ interface InputProps {
   className?: string;
   labelPlacement?: "inside" | "outside" | "outside-left";
   size?: "sm" | "md" | "lg";
+  radius?: "sm" | "md" | "lg" | "none" | "full";
   classNames?:
     | SlotsToClasses<
         | "description"
@@ -64,6 +65,7 @@ export default function ControlledInput<T extends FieldValues>({
           type={props?.type || "text"}
           {...field}
           isDisabled={props?.disabled}
+          radius={props?.radius}
           onBlur={props.onBlur}
           onValueChange={(value) => {
             field.onChange(value);
