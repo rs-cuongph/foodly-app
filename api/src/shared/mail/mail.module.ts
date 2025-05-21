e3a543c43765f776c97bd8226997c649c3623454
cuongph <cuongph@runsystem.net>
2025-05-21 12:23:29 +0700

    Add Bull Board integration in mail module; update package.json and pnpm-lock.yaml dependencies

--- api/src/shared/mail/mail.module.ts
+++ api/src/shared/mail/mail.module.ts
@@ -5,13 +5,18 @@ import { MailConfig } from './mail.config';
 import { MailConsumer } from './mail.consumer';
 import { BullModule } from '@nestjs/bull';
 import { QUEUE_NAME_ENUM } from '@enums/job.enum';
-
+import { BullBoardModule } from '@bull-board/nestjs';
+import { BullAdapter } from '@bull-board/api/bullAdapter';
 @Module({
   imports: [
     MailerModule.forRootAsync({
       useClass: MailConfig,
     }),
     BullModule.registerQueue({ name: QUEUE_NAME_ENUM.MAIL }),
+    BullBoardModule.forFeature({
+      name: 'mail-queue',
+      adapter: BullAdapter,
+    }),
   ],
   providers: [MailService, MailConsumer],
   exports: [MailService],
