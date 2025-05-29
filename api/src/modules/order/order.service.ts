5e19e8da9a472cb07fa9f25332c5191317e942e0
cuongph <cuongph@runsystem.net>
2025-05-30 00:27:07 +0700

    update code

--- api/src/modules/order/order.service.ts
+++ api/src/modules/order/order.service.ts
@@ -98,7 +98,7 @@ export class OrderService {
   private generateUniqueCode() {
     const timestamp = dayjs().format('YYMMDDHHmmss'); // 12 characters
     const randomChars = Math.random().toString(36).substring(2, 10); // 8 characters
-    return `FDY${timestamp}${randomChars}`.toLowerCase().slice(0, 20); // total 20 characters
+    return `FDY${timestamp}${randomChars}`.toUpperCase().slice(0, 20); // total 20 characters
   }
 
   async create(body: CreateOrderDTO, user: RequestWithUser['user']) {
